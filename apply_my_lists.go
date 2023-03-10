9d492a71597f93d3984acc6941cf44e86eac18e5
Torsten Bronger <bronger@physik.rwth-aachen.de>
2023-03-10 21:12:10 +0100

    Switch to simpler data structure
    This is with the hope to reduce the memory usage.

--- apply_my_lists.go
+++ apply_my_lists.go
@@ -19,11 +19,12 @@ func exitOnError(err error, msg string) {
 	os.Exit(1)
 }
 
-var domains = make(map[string]map[string]bool)
+var domains []map[string]bool
 
 const domFilepath = "/tmp/hosts-blacklist"
 
 func readDomains() error {
+	domainsMap := make(map[string]map[string]bool)
 	f, err := os.Open(domFilepath)
 	if err != nil {
 		return pkg_errors.Errorf("Could not open domains file “%v”", domFilepath)
@@ -35,17 +36,19 @@ func readDomains() error {
 		components := strings.Split(domain, ".")
 		length := len(components)
 		tld := components[length-2] + "." + components[length-1]
-		if _, exists := domains[tld]; !exists {
-			domains[tld] = make(map[string]bool)
+		if _, exists := domainsMap[tld]; !exists {
+			domainsMap[tld] = make(map[string]bool)
 		}
-		domains[tld][domain] = true
+		domainsMap[tld][domain] = true
+	}
+	for _, subdomains := range domainsMap {
+		domains = append(domains, subdomains)
 	}
 	return nil
 }
 
-func check(tld string, minimal chan<- string, wg *sync.WaitGroup) {
+func check(subdomains map[string]bool, minimal chan<- string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	subdomains := domains[tld]
 	for domain := range subdomains {
 		for otherDomain := range subdomains {
 			if domain != otherDomain && strings.HasSuffix(domain, otherDomain) {
@@ -64,9 +67,9 @@ func main() {
 	slog.Info("End reading domains", "numberTLDs", len(domains))
 	minimal := make(chan string)
 	var wg sync.WaitGroup
-	for tld := range domains {
+	for _, subdomains := range domains {
 		wg.Add(1)
-		go check(tld, minimal, &wg)
+		go check(subdomains, minimal, &wg)
 	}
 	minimalSet := make(map[string]bool)
 	var wgCollect sync.WaitGroup
